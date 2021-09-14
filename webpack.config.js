20865693c12fab668296819d9242da911524e0ee
liekevdvoort <49523639+liekevdvoort@users.noreply.github.com>
2021-09-14 12:00:42 +0200

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
-const path = require('path');
+const path = require("path");
 var webpack = require("webpack");
 
-module.exports = { 
-  mode: 'development',  
-  devtool: 'source-map',
-  entry: './public/serial_test.js',
-  output: {
-    publicPath: './',
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  externals:{
-    '@brightsign/registry': 'commonjs @brightsign/registry',
-    '@brightsign/storageinfo': 'commonjs @brightsign/storageinfo',
-    '@brightsign/screenshot': 'commonjs @brightsign/screenshot',
-    '@brightsign/messageport': 'commonjs @brightsign/messageport'
-  },
-  module: {
-    rules: [{}]
-  },
-  plugins: [],
-  target: 'node'
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    entry: "./public/node_serial.js",
+    output: {
+        publicPath: "./",
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    externals: {
+        "@brightsign/registry": "commonjs @brightsign/registry",
+        "@brightsign/storageinfo": "commonjs @brightsign/storageinfo",
+        "@brightsign/screenshot": "commonjs @brightsign/screenshot",
+        "@brightsign/messageport": "commonjs @brightsign/messageport",
+    },
+    module: {
+        noParse: function (content) {
+            return /express/.test(content);
+        },
+    },
+    plugins: [],
+    target: "node",
 };
